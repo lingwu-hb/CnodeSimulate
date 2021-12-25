584be155dfb6f6fc4f2a79af9b7d07c3e4601a0c
panningzhi <414179035@qq.com>
2021-12-25 15:36:13 +0800

    feat:入口文件

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
+import { HashRouter, Route, Redirect } from 'react-router-dom'
 import Header from './components/Header/Index'
+import Home from './views/Home/Index'
+import Profile from './views/Profile/Index'
+import Topic from './views/Topic/Index'
 import './App.scss'
 
 /**
@@ -9,7 +13,16 @@ class App extends Component {
     render() {
         return (
             <div>
-                hanbonb
+                <Header />
+                <HashRouter>
+                    <div className="box">
+                        <Route exact path="/" component={Home} />
+                        <Route path="/topic/:id" component={Topic} />
+                        <Route path="/user/:id" component={Profile} />
+                        <Route exact path="/topic" render={() => <Redirect to="/" />} />
+                        <Route exact path="/user" render={() => <Redirect to="/" />} />
+                    </div>
+                </HashRouter>
             </div>
         )
     }
